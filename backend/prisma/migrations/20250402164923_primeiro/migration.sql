5c92b9688afe2cf9e7ac0eda488027ad92712dec
Gabriel de Jesus Silva <gabriel.jesilva@gmail.com>
2025-04-03 16:02:37 -0300

    adding docker and postgres

---
+++ backend/prisma/migrations/20250402164923_primeiro/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "questions" (
+    "id" TEXT NOT NULL,
+    "group" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "firsrtAnswer" TEXT NOT NULL,
+    "secondAnswer" TEXT NOT NULL,
+    "thirdAnswer" TEXT NOT NULL,
+    "fourthAnswer" TEXT NOT NULL,
+    "CorectAnswer" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "questions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "questions_title_key" ON "questions"("title");
